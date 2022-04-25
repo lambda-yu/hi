963cc5c7e7543ce41a5b275065db181f861b63e5
lambda <lambda.yu@hi.com>
2022-04-25 17:50:04 +0800

    command error

--- commands/commands.go
+++ commands/commands.go
@@ -28,7 +28,7 @@ var (
 // transfer subcommand
 var (
 	one2ManyCommand = &cli.Command{
-		Name:   "OneToMany",
+		Name:   "OneToOne",
 		Flags:  config.TransferOne2ManyFlags,
 		Action: transfer.ClientWrapHandle(transfer.HandleOne2One),
 	}
